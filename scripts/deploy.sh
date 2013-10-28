79c8a0154fe17c64985d27765284d8a0b37484a3
Martin Wimpress <code@flexion.org>
2013-10-28 12:16:57 +0100

    Simple deployment script.

---
+++ scripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+NIKOLA_ENV="nikola-611"
+
+echo "Deploying mate-desktop.org"
+
+if [ -d ${HOME}/PythonEnvs/${NIKOLA_ENV} ]; then
+	echo " - Found Nikola stack."
+	source ${HOME}/PythonEnvs/${NIKOLA_ENV}/bin/activate
+else
+	echo " - ERROR! I couldn't find the Nikola stack. Exitting."
+	exit 1
+fi
+
+if [ -d ${HOME}/Websites/mate-desktop.org/.git ]; then
+	echo " - Found mate-desktop.org Git repository. Pulling..."
+	cd ${HOME}/Websites/mate-desktop.org
+	git pull
+	if [ $? -ne 0 ]; then
+		echo " - WARNING! 'git pull' encountered problems."
+	fi
+else
+	echo " - Cloning mate-desktop.org Git repository..."
+	${HOME}/Websites
+	git clone https://github.com/mate-desktop/mate-desktop.org
+	if [ $? -ne 0 ]; then
+		echo " - ERROR! 'git clone' encountered problems. Exitting."
+		exit 1
+	fi
+	cd ${HOME}/Websites/mate-desktop.org
+fi
+
+
+# Optionally deep clean before the build.
+if [ "${1}" == "clean" ]; then
+	nikola clean
+	nikola forget
+fi
+
+nikola build
+nikola deploy
