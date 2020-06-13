71117e42e64f1d0eceaf6283c402f9108a94f0f2
tamplan <tamplan@free.fr>
2020-06-13 18:07:40 +0200

    Fix typo

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = {"en": "The MATE Team", "ca": "L'equip de MATE", "es": "El equipo de MATE", "ca": "L'équipe de MATE", "nl": "Het MATE team"}
+BLOG_AUTHOR = {"en": "The MATE Team", "ca": "L'equip de MATE", "es": "El equipo de MATE", "fr": "L'équipe de MATE", "nl": "Het MATE team"}
 BLOG_TITLE = "MATE"
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
